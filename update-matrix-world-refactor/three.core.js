3e1a08e7ab13d51252e1e96b9e175aacf6cb0387
PoseidonEnergy <moarnow@hotmail.com>
2026-02-01 17:50:24 -0600

    ...

--- update-matrix-world-refactor/three.core.js
+++ update-matrix-world-refactor/three.core.js
@@ -13452,7 +13452,7 @@ let _object3DId = 0;
 
 let _respectMatrixAutoUpdateFlag = false;
 
-console.log( '(special 9)...' );
+console.log( '(special 10)...' );
 
 const _v1$4 = /*@__PURE__*/ new Vector3();
 const _q1 = /*@__PURE__*/ new Quaternion();
