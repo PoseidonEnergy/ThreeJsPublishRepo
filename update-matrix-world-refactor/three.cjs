ff19cb7eed0487a6b05112fbcfb33d33aefc4255
PoseidonEnergy <moarnow@hotmail.com>
2026-02-01 22:06:09 -0600

    ...

--- update-matrix-world-refactor/three.cjs
+++ update-matrix-world-refactor/three.cjs
@@ -13454,7 +13454,7 @@ let _object3DId = 0;
 
 let _respectMatrixAutoUpdateFlag = false;
 
-console.log( '(special 12)...' );
+console.log( '(special 13)...' );
 
 const _v1$4 = /*@__PURE__*/ new Vector3();
 const _q1 = /*@__PURE__*/ new Quaternion();
--- update-matrix-world-refactor/three.core.js
+++ update-matrix-world-refactor/three.core.js
@@ -13452,7 +13452,7 @@ let _object3DId = 0;
 
 let _respectMatrixAutoUpdateFlag = false;
 
-console.log( '(special 12)...' );
+console.log( '(special 13)...' );
 
 const _v1$4 = /*@__PURE__*/ new Vector3();
 const _q1 = /*@__PURE__*/ new Quaternion();
